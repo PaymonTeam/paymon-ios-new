b9cb5f5b51ebfb40f38fa80c5654ccf1c921cda8
Jogendar choudhary <jschoudhary01@gmail.com>
2018-08-04 12:38:18 +0530

    Commit :- Notification controller changed completed.

--- paymon/Controllers/Settings/Notifications/NotificationSoundViewController.swift
+++ paymon/Controllers/Settings/Notifications/NotificationSoundViewController.swift
@@ -3,22 +3,23 @@ import UIKit
 class NotificationSoundViewController: UIViewController {
     
     @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBAction private func onNavBarItemLeftClicked() {
-        dismiss(animated: true)
-    }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Left bar button item added.
         let navigationItem = UINavigationItem()
-
         let leftButton = UIBarButtonItem(image: UIImage(named: "nav_bar_item_arrow_left"), style: .plain, target: self, action: #selector(onNavBarItemLeftClicked))
-
         navigationItem.leftBarButtonItem = leftButton
-
         navigationItem.title = "Sounds".localized
-
         navigationBar.items = [navigationItem]
-
+        
+    }
+    
+    // This Method calls when user clicked on the left bar button Iteam or back button.
+    @objc func onNavBarItemLeftClicked() {
+        
+        dismiss(animated: true)
+        
     }
 }
--- paymon/Controllers/Settings/Notifications/SettingsNotificationViewController.swift
+++ paymon/Controllers/Settings/Notifications/SettingsNotificationViewController.swift
@@ -1,26 +1,26 @@
 import UIKit
 
 class SettingsNotificationViewController : UIViewController {
-
     
     @IBOutlet weak var navigationBar: UINavigationBar!
     
-    @IBAction private func onNavBarItemLeftClicked() {
-        dismiss(animated: true)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Left bar button item added.
         let navigationItem = UINavigationItem()
-
         let leftButton = UIBarButtonItem(image: UIImage(named: "nav_bar_item_arrow_left"), style: .plain, target: self, action: #selector(onNavBarItemLeftClicked))
-
         navigationItem.leftBarButtonItem = leftButton
-
         navigationItem.title = "Notifications".localized
-
         navigationBar.items = [navigationItem]
-
+        
     }
+    
+    // This Method calls when user clicked on the left bar button Iteam or back button.
+    @objc func onNavBarItemLeftClicked() {
+        
+        dismiss(animated: true)
+        
+    }
+    
 }
