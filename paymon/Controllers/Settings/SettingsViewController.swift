869a253356dbbc72f5b83e4a8ea0a2a81fec0780
Jogendar choudhary <jschoudhary01@gmail.com>
2018-08-11 12:46:57 +0530

    Commit : Changes in the security module are completed.

--- paymon/Controllers/Settings/SettingsViewController.swift
+++ paymon/Controllers/Settings/SettingsViewController.swift
@@ -5,23 +5,22 @@ class SettingsViewController : UIViewController {
     @IBOutlet weak var navigationBar: UINavigationBar!
 
     @IBOutlet weak var borderConstraint: NSLayoutConstraint!
-    @IBAction private func onNavBarItemLeftClicked() {
-        dismiss(animated: true)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         borderConstraint.constant = 0.5
         
+        // Left bar button item added.
         let navigationItem = UINavigationItem()
-
         let leftButton = UIBarButtonItem(image: UIImage(named: "nav_bar_item_arrow_left"), style: .plain, target: self, action: #selector(onNavBarItemLeftClicked))
-
         navigationItem.leftBarButtonItem = leftButton
-
         navigationItem.title = "Settings".localized
-
         navigationBar.items = [navigationItem]
+    }
+    
+    // This Method calls when user clicked on the left bar button Iteam or back button.
+    @objc func onNavBarItemLeftClicked() {
+        dismiss(animated: true)
     }
 }
